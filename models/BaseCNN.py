9f83a7aca9ff0f67e68685e781d46d17ab9748cc
Pooria Azami <pooriaazami@gmail.com>
2022-11-12 22:48:38 +0330

    Attention Layer added + code refactor

--- models/BaseCNN.py
+++ models/BaseCNN.py
@@ -4,8 +4,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BaseCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, embedding_size=128):
         super().__init__()
+
+        self.embedding_size = embedding_size
 
         self.conv1 = nn.Conv2d(3, 16, 3, padding=(1, 1)) #(in_channels, out_channels, filter_size, padding=(1, 1)) --> padding (1, 1) is used to prevend shrinking the image size
         self.conv2 = nn.Conv2d(16, 64, 3, padding=(1, 1))
@@ -13,7 +15,7 @@ class BaseCNN(nn.Module):
 
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.embedding = nn.Linear(8192, 128)
+        self.embedding = nn.Linear(8192, self.embedding_size)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
